e7a567b8b519a91c8a898d88f7a73d3fde12c953
massive goose <54300503+massiveg00se@users.noreply.github.com>
2021-12-07 13:29:26 -0800

    rename files

---
+++ load_assets.py
@@ -0,0 +1,13 @@
+import pygame
+import os
+
+CIRCLE = pygame.image.load(
+    os.path.join('assets', 'hitcircle.png'))
+A_CIRCLE = pygame.image.load(
+    os.path.join('assets', 'approachcircle.png'))
+CURSOR = pygame.image.load(
+    os.path.join('assets', 'cursor.png'))
+NUM_1 = pygame.image.load(
+    os.path.join('assets', 'default-1.png'))
+NUM_2 = pygame.image.load(
+    os.path.join('assets', 'default-2.png'))
---
+++ load_hd_assets.py
@@ -0,0 +1,13 @@
+import pygame
+import os
+
+CIRCLE = pygame.image.load(
+    os.path.join('assets', 'hitcircle@2x.png'))
+A_CIRCLE = pygame.image.load(
+    os.path.join('assets', 'approachcircle@2x.png'))
+CURSOR = pygame.image.load(
+    os.path.join('assets', 'cursor@2x.png'))
+NUM_1 = pygame.image.load(
+    os.path.join('assets', 'default-1@2x.png'))
+NUM_2 = pygame.image.load(
+    os.path.join('assets', 'default-2@2x.png'))
