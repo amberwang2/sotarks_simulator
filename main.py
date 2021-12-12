06f7b1d028e105e3f9870fe4a677731a846e9afa
massive goose <massivegoose@protonmail.com>
2021-12-12 01:26:27 -0800

    feat: add alpha fade-in for hit objects
    rewrite of game structure may be needed in order to work around user interaction

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import random
 from constants import *
 import load_assets
+from map_tools import *
 
 # setup
 pygame.init()
@@ -11,9 +12,9 @@ CURSOR_RECT = load_assets.CURSOR.get_rect()
 pygame.display.set_caption('sotarks simulator')
 pygame.mouse.set_visible(False)
 game_status = False
+results_screen = False
 font_48 = pygame.font.Font(os.path.join('assets', 'FiraCode-Regular.ttf'), 48)
 welcome_text = font_48.render("Press enter to start", True, WHITE)
-results_screen = False
 
 def change_status():
     global game_status, current_map, current_time
@@ -55,70 +56,41 @@ def game():
     update_objects(current_map, current_time)
     for i in current_map:
         if i.field:
-            WIN.blit(load_assets.CIRCLE, (i.x - 64, i.y - 64))
+            copy = load_assets.CIRCLE.copy()
+            copy.set_alpha(i.opacity*255)
+            WIN.blit(copy, (i.x - 64, i.y - 64))
             if i.field_time < 600:
                 scaled = pygame.transform.scale(load_assets.A_CIRCLE, (i.approach_mult, i.approach_mult))
+                scaled.set_alpha(i.opacity*255)
                 WIN.blit(scaled, (i.x - (scaled.get_width()//2), i.y - (scaled.get_height()//2)))
+    # for event in pygame.event.get():
+    #     if event.type == pygame.KEYDOWN:
+    #         if event.key == pygame.K_z or event.key == pygame.K_x:
+    #             print("keypress", event.key)
+    #             pos = pygame.mouse.get_pos()
+    #             for i in current_map:
+    #                 if i.hit_window:
+    #                     if (i.x - pos[0])**2 + (i.y - pos[1])**2 < 64**2:
+    #                         precision = abs(i.timing_point - current_time)
+    #                         if precision <= 30:
+    #                             i.score = 300
+    #                         elif precision <= 75:
+    #                             i.score = 100
+    #                         elif precision <= 120:
+    #                             i.score = 50
+    #                         print("HIT ", i.score)
+    #                         i.field = False
     current_time += 10
     # end stuff
     CURSOR_RECT.center = pygame.mouse.get_pos()
     WIN.blit(load_assets.CURSOR, CURSOR_RECT)
     pygame.display.update()
 
-def end_screen():
+def results_screen():
     WIN.fill(BLACK)
     CURSOR_RECT.center = pygame.mouse.get_pos()
     WIN.blit(load_assets.CURSOR, CURSOR_RECT)
     pygame.display.update()
-
-def new_map(n):
-    points = [(random.randint(150, 1130), random.randint(150, 570))]
-
-    for i in range(n - 1):
-        temp = True
-        while temp:
-            point = (random.randint(150, 1130), random.randint(150, 570))
-            dist = ((point[0] - points[-1][0])**2 + (point[1] - points[-1][1])**2)**0.5
-            if dist > 532:
-                points.append(point)
-                temp = False
-
-    return [HitObject(points[i][0], points[i][1], i*600 + 3000) for i in range(len(points))]
-
-'''
-Generate n number of random points
-Each point is at least 532 pixels from the previous point
-points -> list of tuples in (x, y) format
-screen margin of 150 pixels on all sides
-allowed x [150, 1130]
-allowed y [150, 570]
-'''
-
-def update_objects(objects, time):
-    for i in objects:
-        if i.timing_point - 600 == time:
-            i.field = True
-        if i.timing_point + 120 == time:
-            i.field = False
-        if i.field:
-            i.updater()
-
-class HitObject: # circle size 128x128
-    field = False # is the object on field?
-    hit_window = False # is the hit window for the object active?
-    field_time = 0  # time object has been on field in ms
-    hit = False
-    score = None
-
-    def __init__(self, x, y, timing_point):
-        self.x = x
-        self.y = y
-        self.timing_point = timing_point
-
-    def updater(self):
-        self.approach_mult = round(134 * (3 - self.field_time / 200)) if self.field_time <= 400 else 1
-        self.opacity = self.field_time / 400 if self.field_time <= 400 else 1
-        self.field_time += 10
 
 
 if __name__ == "__main__":
